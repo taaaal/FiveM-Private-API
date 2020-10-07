fe2dac02b864f7e90d7f2cc7665b08ab36f6f8bd
youngtaaal <63550727+youngtaaal@users.noreply.github.com>
2020-10-07 04:04:54 +0300

    Update betterfivem.py

--- betterfivem.py
+++ betterfivem.py
@@ -1,153 +1,117 @@
-#import asyncio
+import asyncio
 import json
-#import aiohttp
-import os
-import requests
+import aiohttp
+import re
 
-import datetime
-from datetime import datetime
+'''
+Custom Exceptions
+'''
 
-import base64
-from base64 import b64decode
+class BadIPFormat(Exception):
+    pass
 
+class ServerNotRespond(Exception):
+    pass
+
+'''
+User represented FiveM player
+    `data` -> dict | User's data as a dict
+'''
+
+class User:
+    
+    def __init__(self, data):
+        self.id = data.get('id')
+        self.name = data.get('name')
+        self.ping = data.get('ping')
+    
+        self.steam_id = self.get_clean_id(data['identifiers'].get('steam', 'none'))
+        self.license_id = self.get_clean_id(data['identifiers'].get('license', 'none')
+        self.skype_id = self.get_clean_id(data['identifiers'].get('license', 'none')
+        self.discord_id = self.get_clean_id(data['identifiers'].get('live', 'none'))
+        self.fivem_id = self.get_clean_id(data['identifiers'].get('fivem', 'none'))
+        self.xboxlive_id = self.get_clean_id(data['identifiers'].get('xbl', 'none'))
+        self.ipaddress = self.get_clean_id(data['identifiers'].get('ip', 'none'))
+                       
+   def get_clean_id(self, identifier: str):
+        match = re.match('([a-z]+)\:([a-z0-9]+)', identifier)
+        if not match:
+            return None
+        return match.groups()[1]
+
+'''
+Server represented FiveM Server Wrapper
+    `srvip` -> str       |   Server's IP
+    `max_slots` -> int   |   Server's max players
+'''
+                                          
 class Server:
-    
-    def __init__(self, server_ip: str):
-        self.server_ip = server_ip
-        self.getserver()
-        
-        self.serverstatus = False
-        self.serverinfo = {}
-        self.serverinfo_vars = {}
-        self.serverplayers = {}
-          
-    def getserver(self): 
-        try:
-                serverReq = requests.get(f"http://{self.server_ip}/info.json")
-        except:
-                self.online_players = '0/0'
-                return f"[{datetime.now().strftime('%H:%M:%S')}] Incorrect IP or server is not responding."           
-        else:            
-            serverInfo = json.loads(serverReq.text)
-        
-            playersReq = requests.get(f"http://{self.server_ip}/players.json")
-            serverPlayers = json.loads(playersReq.text)
-                    
-            self.serverinfo = serverInfo
-            self.serverinfo_vars = serverInfo["vars"]
-            self.serverplayers = serverPlayers
-            self.serverstatus = True
-    
-    #def stringToBase64(s):
-    #    return base64.b64encode(s.encode('utf-8'))
+         
+     def __init__(self, srvip, max_slots = 32):
+        self.srvip = srvip if self.check_ip_format(srvip) is True
+        asyncio.get_event_loop().run_until_complete(self.get_players_data())
 
-    #def base64ToString(b):
-    #    return base64.b64decode(b).decode('utf-8')
+        self.max_slots = max_slots 
+        self.status = False
 
-    @property
-    def icon(self):
-        icon = self.serverinfo.get("icon", "This server has no icon.")
-        if icon != 'This server has no icon.':
-            with open("server_icon.png", "wb") as f:
-                f.write(base64.b64decode(icon))
-            icon = f'The server icon has been saved on the API folder: {f.name} | {os.getcwd()}'
-        return icon
-    
-    @property
-    def scripts(self):
-            return self.serverinfo.get("resources", "This server has no scripts.")
+   def __str__(self):
+        pass
 
-    @property   
-    def developers(self):
-            return self.serverinfo_vars.get("Developer", "No developers were specified for this server.") 
+   def __repr__(self):
+        pass       
 
-    @property
-    def discord(self):
-            return self.serverinfo_vars.get("Discord", "No discord server was specified for this server.") 
+    def check_ip_format(self, srvip):
+        part, port = r'([0-9][0-9][0-9])', r'([0-9][0-9][0-9][0-9][0-9])'
+        match = re.match({0}.{0}.{0}.{0}:{1}.format(part, port), srvip)
+        if not match:
+            raise BadIPFormat('[ERROR] Incorrect IP format.')
+        return True
 
-    @property
-    def pubfeed(self):
-            return self.serverinfo_vars.get("activitypubFeed", "No activity pub feed was specified for this server.")
+    async def get_players_data(self)
+        async with aiohttp.ClientSession().get('http://{}/players.json'.format(self.srvip)) as resp:
+            if resp.status != 200:
+               raise ServerNotRespond('[ERROR] Server is not responding or not found.')
 
-    @property
-    def banner_connecting(self):
-            return self.serverinfo_vars.get("banner_connecting", "This server has no banner for server connecting.")
+            data = await resp.read()         
+            self._data = json.loads(data)
+            self.status = True           
 
-    @property
-    def banner_detail(self):
-            return self.serverinfo_vars.get("banner_detail", "This server has no detail banner.")
+   def _players(self):
+        for player in self._data:
+            yield User(player)
 
-    @property
-    def license_key_token(self):
-            return self.serverinfo_vars.get("sv_licenseKeyToken", "No license key token were specified for this server.")
+   @property
+   def online_players(self):
+       return {'online': len(set(self._players)), 'max': self.max_slots}
 
-    @property
-    def max_players(self):
-            return self.serverinfo_vars.get("sv_maxClients", "No information about max players were specified for this server.")
-    
-    @property
-    def online_players(self):
-            return f"{len(self.serverplayers)}/{self.max_players}"
+   #@property
+   #def scripts(self):
+   #       return self.serverinfo.get("resources", "This server has no scripts.")
 
-    @property 
-    def tags(self):
-            tags = self.serverinfo_vars.get("tags", "This server has no tags.")
-            if tags != 'This server has no tags':
-                tags = list(tags.split(','))
-            return tags
+   #@property   
+   #def developers(self):
+   #       return self.serverinfo_vars.get("Developer", "No developers were specified for this server.") 
 
-    @property 
-    def top_tag(self):
-            tags = self.tags
-            if tags != 'This server has no tags':
-                toptag = tags[0]
-            return toptag   
+   #@property
+   #def discord(self):
+   #       return self.serverinfo_vars.get("Discord", "No discord server was specified for this server.") 
 
-    @property
-    def version(self):
-            return self.serverinfo.get("version", "No discord server was specified for this server.")     
+   #@property
+   #def pubfeed(self):
+   #       return self.serverinfo_vars.get("activitypubFeed", "No activity pub feed was specified for this server.")
 
-    @property
-    def anticheat(self):
-            return self.serverinfo_vars.get("Anticheat", "Disabled")   
+   #@property
+   #def banner_connecting(self):
+   #       return self.serverinfo_vars.get("banner_connecting", "This server has no banner for server connecting.")
 
-    @property
-    def community(self):
-            return self.serverinfo_vars.get("Community", "No community server was specified for this server.")   
+   #@property
+   #def banner_detail(self):
+   #       return self.serverinfo_vars.get("banner_detail", "This server has no detail banner.")
 
-    @property
-    def txt(self):
-            f = open(f"./serverinfo.txt", "w")
-            f.write("""  
-            ______ _____ _   _ ________  ___    ___  ______ _____ 
-            |  ___|_   _| | | |  ___|  \/  |   / _ \ | ___ \_   _|
-            | |_    | | | | | | |__ | .  . |  / /_\ \| |_/ / | |  
-            |  _|   | | | | | |  __|| |\/| |  |  _  ||  __/  | |  
-            | |    _| |_\ \_/ / |___| |  | |  | | | || |    _| |_ 
-            \_|    \___/ \___/\____/\_|  |_/  \_| |_/\_|    \___/ 
-            """)
-            f.write(f"\n            Basic information about {self.serverip}")
-            f.write("\n-------------------------------------------------------------------------------------------------------------------")
-            f.write(f"\nServer community: {self.community}")
-            f.write(f"\nServer players: {len(self.serverplayers)}/{self.max_players}")
-            f.write(f"\nServer scripts: {len(self.scripts)}")
-            f.write(f"\nServer developers: {self.developers}")
-            f.write(f"\nServer tags: {', '.join(self.tags)}")
-            f.write(f"\nServer top tag: {self.top_tag}")
-            f.write(f"\nServer pubfeed: {self.pubfeed}")
-            f.write("\n-------------------------------------------------------------------------------------------------------------------")
-            f.write(f"\n            Links information about {self.serverip}")
-            f.write("\n-------------------------------------------------------------------------------------------------------------------")
-            f.write(f"\nServer discord (invite url link): {self.discord}")
-            f.write(f"\nServer banner connecting (image url link): {self.banner_connecting}")
-            f.write(f"\nServer banner detail (image url link): {self.banner_detail}")
-            f.write("\n-------------------------------------------------------------------------------------------------------------------")
-            f.write(f"\n            More information about {self.serverip}")
-            f.write("\n-------------------------------------------------------------------------------------------------------------------")
-            f.write(f"\nServer players slots: {self.max_players}")
-            f.write(f"\nServer version: v{self.version}")
-            f.write(f"\nServer anticheat: {self.anticheat}")
-            f.write(f"\nServer license key token: {self.license_key_token}")
-            f.write("\n-------------------------------------------------------------------------------------------------------------------")
-            f.close()
-            return f'The server information has been created on the API folder: {f.name} | {os.getcwd()}'
+   #@property
+   #def license_key_token(self):
+   #       return self.serverinfo_vars.get("sv_licenseKeyToken", "No license key token were specified for this server.")
+
+   #@property
+   #def max_players(self):
+   #       return self.serverinfo_vars.get("sv_maxClients", "No information about max players were specified for this server.")
