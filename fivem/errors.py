52ab92c84e63becb6fe37589d22e867c5d21baff
youngtaaal <63550727+youngtaaal@users.noreply.github.com>
2020-10-09 18:09:23 +0300

    Create errors.py

---
+++ fivem/errors.py
@@ -0,0 +1,11 @@
+class BadIPFormat(Exception):
+    '''
+    Invalid IP format 
+    '''
+    pass
+
+class ServerNotRespond(Exception):
+    '''
+    Server not respond or not found 
+    '''
+    pass
