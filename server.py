a8ef19e92eaa223caeaa40ae44a07003a0ce2dc4
youngtaaal <63550727+youngtaaal@users.noreply.github.com>
2020-10-09 18:05:46 +0300

    Update and rename betterfivem.py to server.py

--- server.py
+++ server.py
@@ -14,41 +14,6 @@ class ServerNotRespond(Exception):
     Server not respond or not found Exception
     '''
     pass
-
-class User:
-    
-    def __init__(self, data):
-        '''
-        User represented by FiveM player
-        `data` -> dict | User's data as a dict
-        '''
-        self.id = data.get('id')
-        self.name = data.get('name')
-        self.ping = data.get('ping')
-    
-        based_identifiers = ('steam', 'licenese', 'discord', 'fivem') 
-        self.sort_identifiers(based_identifiers, data['identifiers'])
-                              
-        self.steam_id = self.sorted_identifiers.get('steam_id')
-        self.license_id = self.sorted_identifiers.get('license_id')
-        self.discord_id = self.sorted_identifiers.get('discord_id')
-        self.fivem_id = self.sorted_identifiers.get('fivem_id')
-    
-    def sort_identifiers(self, based_identifiers, data_identifiers):
-        self.sorted_identifiers = dict()
-        for k in based_identifiers:
-            for v in data_identifiers:
-                if v.startswith(k):
-                    clean_v = self.get_clean_id(v)
-                    clean_k = k + '_id'
-                    self.sorted_identifiers[clean_k] = clean_v
-                    continue    
-                       
-    def get_clean_id(self, identifier):
-        match = re.match('([a-z]+)\:([a-z0-9]+)', identifier)
-        if not match:
-            return None
-        return match.groups()[1]
                                   
 class Server:
          
