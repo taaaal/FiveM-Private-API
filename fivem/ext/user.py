58ba70b5a48a49c0b2f65fbb5dda628e595dfd09
Tal K <63550727+taaaal@users.noreply.github.com>
2024-03-02 13:58:02 +0200

    Update user.py

--- fivem/ext/user.py
+++ fivem/ext/user.py
@@ -11,26 +11,28 @@ class User:
         self.name = data.get('name')
         self.ping = data.get('ping')
     
-        based_identifiers = ('steam', 'licenese', 'discord', 'fivem') 
-        self.sort_identifiers(based_identifiers, data.get('identifiers'))
+        base_identifiers = ('steam', 'license', 'discord', 'fivem') 
+        data_identifiers = data.get('identifiers')
+        
+        sorted_identifiers = self.sort_identifiers(base_identifiers, data_identifiers)
                               
-        self.steam_id   = self.sorted_identifiers.get('steam_id')
-        self.license_id = self.sorted_identifiers.get('license_id')
-        self.discord_id = self.sorted_identifiers.get('discord_id')
-        self.fivem_id   = self.sorted_identifiers.get('fivem_id')
+        self.steam_id   = sorted_identifiers.get('steam_id')
+        self.license_id = sorted_identifiers.get('license_id')
+        self.discord_id = sorted_identifiers.get('discord_id')
+        self.fivem_id   = sorted_identifiers.get('fivem_id')
     
     def sort_identifiers(self, based_identifiers, data_identifiers):
-        self.sorted_identifiers = dict()
-        for k in based_identifiers:
-            for v in data_identifiers:
-                if v.startswith(k):
-                    clean_v = self.get_clean_id(v)
-                    clean_k = k + '_id'
-                    self.sorted_identifiers[clean_k] = clean_v
-                    continue    
+        sorted_identifiers = dict()
+        for base_id in base_identifiers:
+            for identifier in data_identifiers:
+                if identifier.startswith(base_id):
+                    proper_identifier = base_id + '_id'
+                    clean_identifier_value = self.get_clean_id(identifier)
+                    sorted_identifiers[proper_identifier] = clean_identifier_value
+        return sorted_identifiers   
                        
     def get_clean_id(self, identifier):
         match = re.match('([a-z]+)\:([a-z0-9]+)', identifier)
         if not match:
             return None
-        return match.groups()[1]
+        return match.groups(2)
